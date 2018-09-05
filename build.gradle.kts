04294e0b90e5d19ab2f822701fc3d0bf8bb7bdab
IIBUN Toshiyuki <toshiyuki.iibun@gmail.com>
2018-09-06 00:52:50 +0900

    add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version "1.2.61"
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("software.amazon.awscdk:cdk:0.8.2")
+}
+
+kotlin.experimental.coroutines = Coroutines.ENABLE
+
+application {
+    mainClassName = "samples.HelloAwsCdkKt"
+}
